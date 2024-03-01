416cc2ada1ce28f04aa17c217e5ec527448a237e
rezaakbar35 <rezaakbarattariq@gmail.com>
2024-03-02 00:42:59 +0700

    final check

--- model/user.go
+++ model/user.go
@@ -1,13 +1,18 @@
 package model
 
 import (
+	"github.com/asaskevich/govalidator"
 	"gorm.io/gorm"
 )
 
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
 type User struct {
 	gorm.Model
-	Username 	string 		`json:"username"`
-	Email 		string 		`json:"email"`
-	Password 	string 		`json:"password"`
-	Photos 		[]Photo 	`json:"photos" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Username string  `json:"username"`
+	Email    string  `json:"email" valid:"email" gorm:"unique"`
+	Password string  `json:"password" valid:"length(6|16)"`
+	Photos   []Photo `json:"photos" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
